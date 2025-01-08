e9353b0f8411b3164cc69c22f5f3ce30a41847b6
tobezero01 <ducnhuad@gmail.com>
2025-01-09 02:38:16 +0700

    first commit

---
+++ CS_Practice/CS_Practice/Array/main_array.cs
@@ -0,0 +1,36 @@
+﻿using CS_Practice.Sorting;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CS_Practice.Array
+{
+	internal class main_array
+	{
+
+		static void Main(string[] args)
+		{
+			Console.OutputEncoding = System.Text.Encoding.UTF8;
+			int[] arr = { 5, 1, 4, 2, 8 }; // Mảng chưa sắp xếp
+
+			Console.WriteLine("Mảng ban đầu:");
+			PrintArray(arr);
+
+			bubble.BubbleSort(arr); // Gọi phương thức Bubble Sort để sắp xếp mảng
+
+			Console.WriteLine("\nMảng sau khi sắp xếp:");
+			PrintArray(arr); // In ra mảng đã sắp xếp
+		}
+
+		static void PrintArray(int[] arr)
+		{
+			foreach (int item in arr)
+			{
+				Console.Write(item + " ");
+			}
+			Console.WriteLine();
+		}
+	}
+}
---
+++ CS_Practice/CS_Practice/Sorting/bubble.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CS_Practice.Sorting
+{
+	public class bubble
+	{
+		public static void BubbleSort(int[] arr)
+		{
+			int n = arr.Length;
+			for (int i = 0; i < n - 1; i++) // Lặp qua các phần tử mảng
+			{
+				for (int j = 0; j < n - i - 1; j++) // So sánh từng cặp phần tử liền kề
+				{
+					if (arr[j] > arr[j + 1]) // Nếu phần tử trước lớn hơn phần tử sau, đổi chỗ chúng
+					{
+						// Hoán đổi arr[j] và arr[j + 1]
+						int temp = arr[j];
+						arr[j] = arr[j + 1];
+						arr[j + 1] = temp;
+					}
+				}
+			}
+		}
+
+
+		static void Main(string[] args)
+		{
+			
+		}
+	}
+}
